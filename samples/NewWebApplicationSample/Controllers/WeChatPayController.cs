ab6a992ff94f425dd3dfb02259be75d6b9ffd198
Roc <roc@essenol.com>
2018-07-16 18:11:26 +0800

    Update WeChatPayController.cs

--- samples/NewWebApplicationSample/Controllers/WeChatPayController.cs
+++ samples/NewWebApplicationSample/Controllers/WeChatPayController.cs
@@ -159,8 +159,9 @@ namespace NewWebApplicationSample.Controllers
                 TradeType = viewModel.TradeType,
             };
             var response = await _client.ExecuteAsync(request);
-            ViewData["response"] = response.Body;
-            return View();
+            
+            // mweb_url为拉起微信支付收银台的中间页面，可通过访问该url来拉起微信客户端，完成支付,mweb_url的有效期为5分钟。
+            return Redirect(response.MwebUrl);
         }
 
         [HttpGet]
