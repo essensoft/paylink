2b61fe89c51357b4b415e1d29bb9750b49a4ac03
Roc <roc@essensoft.cn>
2018-09-14 14:12:24 +0800

    删除旧版本示例

--- samples/WebApplicationSample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationSample.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
