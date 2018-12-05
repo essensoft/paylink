c36b3c9282c9eb6f6b89996efe86c8e8baae082f
Roc <roc@essensoft.cn>
2018-12-06 01:17:48 +0800

    支持 企业微信 - 发放企业红包

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPaySendWorkWxRedPackRequest.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Essensoft.AspNetCore.Payment.WeChatPay.Response;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
+{
+    /// <summary>
+    /// 发放企业红包
+    /// </summary>
+    public class WeChatPaySendWorkWxRedPackRequest : IWeChatPayCertificateRequest<WeChatPaySendWorkWxRedPackResponse>
+    {
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
+        public string MchBillNo { get; set; }
+
+        /// <summary>
+        /// 公众账号appid
+        /// </summary>
+        public string WxAppId { get; set; }
+
+        /// <summary>
+        /// 发送者名称
+        /// </summary>
+        public string SenderName { get; set; }
+
+        /// <summary>
+        /// 发送红包的应用id
+        /// </summary>
+        public string AgentId { get; set; }
+
+        /// <summary>
+        /// 发送者头像
+        /// </summary>
+        public string SenderHeaderMediaId { get; set; }
+
+        /// <summary>
+        /// 用户openid
+        /// </summary>
+        public string ReOpenId { get; set; }
+
+        /// <summary>
+        /// 金额
+        /// </summary>
+        public int TotalAmount { get; set; }
+
+        /// <summary>
+        /// 红包祝福语
+        /// </summary>
+        public string Wishing { get; set; }
+
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string ActName { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 场景id
+        /// </summary>
+        public string SceneId { get; set; }
+
+        #region IWeChatPayCertificateRequest Members
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendworkwxredpack";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WeChatPayDictionary
+            {
+                { "mch_billno", MchBillNo },
+                { "wxappid", WxAppId },
+                { "sender_name", SenderName },
+                { "agentid", AgentId },
+                { "sender_header_media_id", SenderHeaderMediaId },
+                { "re_openid", ReOpenId },
+                { "total_amount", TotalAmount },
+                { "wishing", Wishing },
+                { "act_name", ActName },
+                { "remark", Remark },
+                { "scene_id", SceneId },
+            };
+            return parameters;
+        }
+
+        public bool IsCheckResponseSign()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Response/WeChatPaySendWorkWxRedPackResponse.cs
@@ -0,0 +1,96 @@
+﻿using System.Xml.Serialization;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Response
+{
+    [XmlRoot("xml")]
+    internal class WeChatPaySendWorkWxRedPackResponse : WeChatPayResponse
+    {
+        /// <summary>
+        /// 返回状态码
+        /// 此字段是通信标识，非交易标识，
+        /// 交易是否成功需要查看result_code来判断
+        /// </summary>
+        [XmlElement("return_code")]
+        public string ReturnCode { get; set; }
+
+        /// <summary>
+        /// 返回信息，如非空，为错误原因
+        /// 签名失败
+        /// 参数格式校验错误
+        /// </summary>
+        [XmlElement("return_msg")]
+        public string ReturnMsg { get; set; }
+
+        /// <summary>
+        /// 签名
+        /// </summary>
+        [XmlElement("sign")]
+        public string Sign { get; set; }
+
+        /// <summary>
+        /// 业务结果
+        /// </summary>
+        [XmlElement("result_code")]
+        public string ResultCode { get; set; }
+
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        [XmlElement("err_code")]
+        public string ErrCode { get; set; }
+
+        /// <summary>
+        /// 错误代码描述
+        /// </summary>
+        [XmlElement("err_code_des")]
+        public string ErrCodeDes { get; set; }
+
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
+        [XmlElement("mch_billno")]
+        public string MchBillNo { get; set; }
+
+        /// <summary>
+        /// 商户号
+        /// </summary>
+        [XmlElement("mch_id")]
+        public string MchId { get; set; }
+
+        /// <summary>
+        /// 公众账号ID
+        /// </summary>
+        [XmlElement("wxappid")]
+        public string WxAppId { get; set; }
+
+        /// <summary>
+        /// 用户openid
+        /// </summary>
+        [XmlElement("re_openid")]
+        public string ReOpenId { get; set; }
+
+        /// <summary>
+        /// 付款金额
+        /// </summary>
+        [XmlElement("total_amount")]
+        public string TotalAmount { get; set; }
+
+        /// <summary>
+        /// 微信单号
+        /// </summary>
+        [XmlElement("send_listid")]
+        public string SendListId { get; set; }
+
+        /// <summary>
+        /// 发送者名称
+        /// </summary>
+        [XmlElement("sender_name")]
+        public string SenderName { get; set; }
+
+        /// <summary>
+        /// 发送者头像
+        /// </summary>
+        [XmlElement("sender_header_media_id")]
+        public string SenderHeaderMediaId { get; set; }
+    }
+}
