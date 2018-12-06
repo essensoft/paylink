0cc2cd74fc2c72b4a57d9b433ef2b0c273a64c7a
Roc <roc@essenol.com>
2018-12-06 19:39:22 +0800

    1. [Security] 调整 AES/AES_CTR_NoPadding/TripleDES(原DES3) 2. 其他适配调整. 3.发布版本时才生成包.

--- src/Essensoft.AspNetCore.Payment.Security/AES.cs
+++ src/Essensoft.AspNetCore.Payment.Security/AES.cs
@@ -4,75 +4,72 @@ using System.Text;
 
 namespace Essensoft.AspNetCore.Payment.Security
 {
-    public enum AESCipherMode
-    {
-        CBC = CipherMode.CBC,
-        ECB = CipherMode.ECB
-    }
-
-    public enum AESPaddingMode
-    {
-        PKCS7 = PaddingMode.PKCS7
-    }
-
     public class AES
     {
-        public static string Encrypt(string data, string key, byte[] iv, AESCipherMode cipherMode, AESPaddingMode paddingMode)
+        public static string Encrypt(string data, string key, byte[] iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            var rm = new RijndaelManaged
+            using (var aes = Aes.Create())
             {
-                IV = iv,
-                Key = Encoding.UTF8.GetBytes(key),
-                Mode = (CipherMode)cipherMode,
-                Padding = (PaddingMode)paddingMode
-            };
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                aes.Mode = cipherMode;
+                aes.Padding = paddingMode;
 
-            var content = Encoding.UTF8.GetBytes(data);
-            var ctf = rm.CreateEncryptor();
-            return Convert.ToBase64String(ctf.TransformFinalBlock(content, 0, content.Length));
+                using (var ctf = aes.CreateEncryptor())
+                {
+                    var content = Encoding.UTF8.GetBytes(data);
+                    return Convert.ToBase64String(ctf.TransformFinalBlock(content, 0, content.Length));
+                }
+            }
         }
 
-        public static string Decrypt(string data, string key, byte[] iv, AESCipherMode cipherMode, AESPaddingMode paddingMode)
+        public static string Decrypt(string data, string key, byte[] iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            var rm = new RijndaelManaged
+            using (var aes = Aes.Create())
             {
-                IV = iv,
-                Key = Encoding.UTF8.GetBytes(key),
-                Mode = (CipherMode)cipherMode,
-                Padding = (PaddingMode)paddingMode
-            };
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                aes.Mode = cipherMode;
+                aes.Padding = paddingMode;
 
-            var content = Convert.FromBase64String(data);
-            var ctf = rm.CreateDecryptor();
-            return Encoding.UTF8.GetString(ctf.TransformFinalBlock(content, 0, content.Length));
+                using (var ctf = aes.CreateDecryptor())
+                {
+                    var content = Convert.FromBase64String(data);
+                    return Encoding.UTF8.GetString(ctf.TransformFinalBlock(content, 0, content.Length));
+                }
+            }
         }
 
-        public static string Encrypt(string data, string key, AESCipherMode cipherMode, AESPaddingMode paddingMode)
+        public static string Encrypt(string data, string key, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            var rm = new RijndaelManaged
+            using (var aes = Aes.Create())
             {
-                Key = Encoding.UTF8.GetBytes(key),
-                Mode = (CipherMode)cipherMode,
-                Padding = (PaddingMode)paddingMode
-            };
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.Mode = cipherMode;
+                aes.Padding = paddingMode;
 
-            var content = Encoding.UTF8.GetBytes(data);
-            var ctf = rm.CreateEncryptor();
-            return Convert.ToBase64String(ctf.TransformFinalBlock(content, 0, content.Length));
+                using (var ctf = aes.CreateEncryptor())
+                {
+                    var content = Encoding.UTF8.GetBytes(data);
+                    return Convert.ToBase64String(ctf.TransformFinalBlock(content, 0, content.Length));
+                }
+            }
         }
 
-        public static string Decrypt(string data, string key, AESCipherMode cipherMode, AESPaddingMode paddingMode)
+        public static string Decrypt(string data, string key, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            var rm = new RijndaelManaged
+            using (var aes = Aes.Create())
             {
-                Key = Encoding.UTF8.GetBytes(key),
-                Mode = (CipherMode)cipherMode,
-                Padding = (PaddingMode)paddingMode
-            };
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.Mode = cipherMode;
+                aes.Padding = paddingMode;
 
-            var content = Convert.FromBase64String(data);
-            var ctf = rm.CreateDecryptor();
-            return Encoding.UTF8.GetString(ctf.TransformFinalBlock(content, 0, content.Length));
+                using (var ctf = aes.CreateDecryptor())
+                {
+                    var content = Convert.FromBase64String(data);
+                    return Encoding.UTF8.GetString(ctf.TransformFinalBlock(content, 0, content.Length));
+                }
+            }
         }
     }
 }
