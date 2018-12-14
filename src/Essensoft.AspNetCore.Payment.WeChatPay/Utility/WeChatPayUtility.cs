b63c9a2174bc2878cd4819dc10651da3316c9c29
Roc <roc@essensoft.cn>
2018-12-14 17:09:54 +0800

    [WeChatPay.Utility] 优化 GetTimeStamp

--- src/Essensoft.AspNetCore.Payment.WeChatPay/Utility/WeChatPayUtility.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Utility/WeChatPayUtility.cs
@@ -29,8 +29,7 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.Utility
 
         public static string GetTimeStamp()
         {
-            var ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return Convert.ToInt64(ts.TotalSeconds).ToString();
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
         }
     }
 }
