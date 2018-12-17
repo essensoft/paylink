bdd005e2a5ae40fb06c5398f73f47cdf4ee68ba4
Roc <roc@essensoft.cn>
2018-12-17 16:27:51 +0800

    [WeChatPay] 新增 酒店押金 - 支付押金接口

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayDepositMicroPayRequest.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using Essensoft.AspNetCore.Payment.WeChatPay.Response;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
+{
+    /// <summary>
+    /// 支付押金
+    /// </summary>
+    public class WeChatPayDepositMicroPayRequest : IWeChatPayRequest<WeChatPayDepositMicroPayResponse>
+    {
+        /// <summary>
+        /// 是否押金支付
+        /// </summary>
+        public string Deposit { get; set; } = "Y";
+
+        /// <summary>
+        /// 应用ID
+        /// </summary>
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// 子商户公众账号ID
+        /// </summary>
+        public string SubAppId { get; set; }
+
+        /// <summary>
+        /// 子商户号
+        /// </summary>
+        public string SubMchId { get; set; }
+
+        /// <summary>
+        /// 设备号
+        /// </summary>
+        public string DeviceInfo { get; set; }
+
+        /// <summary>
+        /// 商品描述
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// 商品详情
+        /// </summary>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 附加数据
+        /// </summary>
+        public string Attach { get; set; }
+
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
+        public string OutTradeNo { get; set; }
+
+        /// <summary>
+        /// 总金额
+        /// </summary>
+        public int TotalFee { get; set; }
+
+        /// <summary>
+        /// 货币类型
+        /// </summary>
+        public string FeeType { get; set; }
+
+        /// <summary>
+        /// 终端IP
+        /// </summary>
+        public string SpbillCreateIp { get; set; }
+
+        /// <summary>
+        /// 订单优惠标记
+        /// </summary>
+        public string GoodsTag { get; set; }
+
+        /// <summary>
+        /// 指定支付方式
+        /// </summary>
+        public string LimitPay { get; set; }
+
+        /// <summary>
+        /// 交易起始时间
+        /// </summary>
+        public string TimeStart { get; set; }
+
+        /// <summary>
+        /// 交易结束时间
+        /// </summary>
+        public string TimeExpire { get; set; }
+
+        /// <summary>
+        /// 授权码
+        /// </summary>
+        public string AuthCode { get; set; }
+
+        /// <summary>
+        /// 场景信息
+        /// </summary>
+        public string SceneInfo { get; set; }
+
+        #region IWeChatPayRequest Members
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/deposit/micropay";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WeChatPayDictionary
+            {
+                { "deposit", Deposit },
+                { "appid", AppId },
+                { "sub_appid", SubAppId },
+                { "sub_mch_id", SubMchId },
+                { "device_info", DeviceInfo },
+                { "body", Body },
+                { "detail", Detail },
+                { "attach", Attach },
+                { "out_trade_no", OutTradeNo },
+                { "total_fee", TotalFee },
+                { "fee_type", FeeType },
+                { "spbill_create_ip", SpbillCreateIp },
+                { "goods_tag", GoodsTag },
+                { "limit_pay", LimitPay },
+                { "time_start", TimeStart },
+                { "time_expire", TimeExpire },
+                { "auth_code", AuthCode },
+                { "scene_info", SceneInfo }
+            };
+            return parameters;
+        }
+
+        public bool IsCheckResponseSign()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
