064d3b3e5d48f099a26dc68ab12baf6d3c7262a2
Roc <roc@essenol.com>
2018-12-30 04:50:10 +0800

    注释

--- src/Essensoft.AspNetCore.Payment.Alipay/IAlipayClient.cs
+++ src/Essensoft.AspNetCore.Payment.Alipay/IAlipayClient.cs
@@ -68,6 +68,7 @@ namespace Essensoft.AspNetCore.Payment.Alipay
         /// <typeparam name="T">领域对象</typeparam>
         /// <param name="request">具体的Alipay API请求</param>
         /// <param name="session">用户会话码</param>
+        /// <param name="reqMethod">请求访问方法</param>
         /// <returns>领域对象</returns>
         Task<T> PageExecuteAsync<T>(IAlipayRequest<T> request, string session, string reqMethod) where T : AlipayResponse;
 
@@ -78,6 +79,7 @@ namespace Essensoft.AspNetCore.Payment.Alipay
         /// <param name="request">具体的Alipay API请求</param>
         /// <param name="optionsName">配置选项名称</param>
         /// <param name="session">用户会话码</param>
+        /// <param name="reqMethod">请求访问方法</param>
         /// <returns>领域对象</returns>
         Task<T> PageExecuteAsync<T>(IAlipayRequest<T> request, string optionsName, string session, string reqMethod) where T : AlipayResponse;
 
--- src/Essensoft.AspNetCore.Payment.UnionPay/IUnionPayClient.cs
+++ src/Essensoft.AspNetCore.Payment.UnionPay/IUnionPayClient.cs
@@ -38,7 +38,6 @@ namespace Essensoft.AspNetCore.Payment.UnionPay
         /// <typeparam name="T">领域对象</typeparam>
         /// <param name="request">具体的UnionPay API请求</param>
         /// <param name="optionsName">配置选项名称</param>
-        /// <param name="reqMethod">请求类型(POST/GET)</param>
         /// <returns>领域对象</returns>
         Task<T> PageExecuteAsync<T>(IUnionPayRequest<T> request, string optionsName) where T : UnionPayResponse;
     }
