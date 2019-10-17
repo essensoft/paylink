94110f7d3abe0c9cd0309248b1ec68078811ae72
Roc <roc@essensoft.cn>
2019-10-17 17:17:25 +0800

    新增 AlipayTradePayNotify

---
+++ src/Essensoft.AspNetCore.Payment.Alipay/Notify/AlipayTradePayNotify.cs
@@ -0,0 +1,199 @@
+﻿using Newtonsoft.Json;
+
+namespace Essensoft.AspNetCore.Payment.Alipay.Notify
+{
+    /// <summary>
+    /// 支付异步通知
+    /// 根据文档：https://opensupport.alipay.com/support/knowledge/36780/201602409820?ant_source=zsearch
+    /// 参考PDF：https://gw.alipayobjects.com/os/rmsportal/UkoSRNhRZnSjQciiPFBu.pdf
+    /// 第10页异步通知
+    /// </summary>
+    public class AlipayTradePayNotify : AlipayNotify
+    {
+        /// <summary>
+        /// 通知时间
+        /// </summary>
+        [JsonProperty("notify_time")]
+        public string NotifyTime { get; set; }
+
+        /// <summary>
+        /// 通知类型
+        /// </summary>
+        [JsonProperty("notify_type")]
+        public string NotifyType { get; set; }
+
+        /// <summary>
+        /// 通知校验ID
+        /// </summary>
+        [JsonProperty("notify_id")]
+        public string NotifyId { get; set; }
+
+        /// <summary>
+        /// 支付宝分配给开发者的应用Id
+        /// </summary>
+        [JsonProperty("app_id")]
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// 支付宝分配给商户的应用 Id
+        /// </summary>
+        [JsonProperty("auth_app_id")]
+        public string AuthAppId { get; set; }
+
+        /// <summary>
+        /// 签名类型
+        /// </summary>
+        [JsonProperty("sign_type")]
+        public string SignType { get; set; }
+
+        /// <summary>
+        /// 签名
+        /// </summary>
+        [JsonProperty("sign")]
+        public string Sign { get; set; }
+
+        /// <summary>
+        /// 支付宝交易号
+        /// </summary>
+        [JsonProperty("trade_no")]
+        public string TradeNo { get; set; }
+
+        /// <summary>
+        /// 商户订单号
+        /// </summary>
+        [JsonProperty("out_trade_no")]
+        public string OutTradeNo { get; set; }
+
+        /// <summary>
+        /// 商户业务号
+        /// </summary>
+        [JsonProperty("out_biz_no")]
+        public string OutBizNo { get; set; }
+
+        /// <summary>
+        /// 买家支付宝用户号
+        /// </summary>
+        [JsonProperty("buyer_id")]
+        public string BuyerId { get; set; }
+
+        /// <summary>
+        /// 买家支付宝账号
+        /// </summary>
+        [JsonProperty("buyer_logon_id")]
+        public string BuyerLogonId { get; set; }
+
+        /// <summary>
+        /// 卖家支付宝用户号
+        /// </summary>
+        [JsonProperty("seller_id")]
+        public string SellerId { get; set; }
+
+        /// <summary>
+        /// 卖家支付宝账号
+        /// </summary>
+        [JsonProperty("seller_email")]
+        public string SellerEmail { get; set; }
+
+        /// <summary>
+        /// 交易状态
+        /// </summary>
+        [JsonProperty("trade_status")]
+        public string TradeStatus { get; set; }
+
+        /// <summary>
+        /// 订单金额
+        /// </summary>
+        [JsonProperty("total_amount")]
+        public string TotalAmount { get; set; }
+
+        /// <summary>
+        /// 实收金额
+        /// </summary>
+        [JsonProperty("receipt_amount")]
+        public string ReceiptAmount { get; set; }
+
+        /// <summary>
+        /// 开票金额
+        /// </summary>
+        [JsonProperty("invoice_amount")]
+        public string InvoiceAmount { get; set; }
+
+        /// <summary>
+        /// 付款金额
+        /// </summary>
+        [JsonProperty("buyer_pay_amount")]
+        public string BuyerPayAmount { get; set; }
+
+        /// <summary>
+        /// 集分宝金额
+        /// </summary>
+        [JsonProperty("point_amount")]
+        public string PointAmount { get; set; }
+
+        /// <summary>
+        /// 总退款金额
+        /// </summary>
+        [JsonProperty("refund_fee")]
+        public string RefundFee { get; set; }
+
+        /// <summary>
+        /// 订单标题
+        /// </summary>
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// 商品描述
+        /// </summary>
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// 交易创建时间
+        /// </summary>
+        [JsonProperty("gmt_create")]
+        public string GmtCreate { get; set; }
+
+        /// <summary>
+        /// 交易付款时间
+        /// </summary>
+        [JsonProperty("gmt_payment")]
+        public string GmtPayment { get; set; }
+
+        /// <summary>
+        /// 交易退款时间
+        /// </summary>
+        [JsonProperty("gmt_refund")]
+        public string GmtRefund { get; set; }
+
+        /// <summary>
+        /// 交易结束时间
+        /// </summary>
+        [JsonProperty("gmt_close")]
+        public string GmtClose { get; set; }
+
+        /// <summary>
+        /// 支付金额信息
+        /// </summary>
+        [JsonProperty("fund_bill_list")]
+        public string FundBillList { get; set; }
+
+        /// <summary>
+        /// 优惠券信息
+        /// </summary>
+        [JsonProperty("voucher_detail_list")]
+        public string VoucherDetailList { get; set; }
+
+        /// <summary>
+        /// 本次交易支付所使用的单品券优惠的商品优惠信息
+        /// </summary>
+        [JsonProperty("discount_goods_detail")]
+        public string DiscountGoodsDetail { get; set; }
+
+        /// <summary>
+        /// 预授权支付模式，该参数仅在信用预授权支付场景下返回。信用预授权支付：CREDIT_PREAUTH_PAY
+        /// </summary>
+        [JsonProperty("auth_trade_pay_mode")]
+        public string AuthTradePayMode { get; set; }
+    }
+}
