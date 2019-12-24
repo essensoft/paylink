1429aedb3d78a76241f8d97dd39aad9ca3824169
Roc <roc@essensoft.cn>
2019-12-25 01:45:33 +0800

    [Security] 支持 GBK

---
+++ src/Essensoft.AspNetCore.Payment.Security/InternalEncoding.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Essensoft.AspNetCore.Payment.Security
+{
+    internal static class InternalEncoding
+    {
+        static InternalEncoding()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        internal static Encoding GetEncoding(string name)
+        {
+            return Encoding.GetEncoding(name);
+        }
+
+        internal static Encoding GetEncoding(int codepage)
+        {
+            return Encoding.GetEncoding(codepage);
+        }
+    }
+}
