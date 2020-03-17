01a0d4fb437894f77c8132881a7bdfe85d6304ac
zlyxm <928310611@qq.com>
2020-03-17 16:00:12 +0800

    Configuration file acquisition failed
    Add extensions with reference to ABP

---
+++ samples/WebApplicationSample/AppConfigurations.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Configuration;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace WebApplicationSample
+{
+    public static class AppConfigurations
+    {
+        private static readonly ConcurrentDictionary<string, IConfigurationRoot> _configurationCache;
+
+        static AppConfigurations()
+        {
+            _configurationCache = new ConcurrentDictionary<string, IConfigurationRoot>();
+        }
+
+        public static IConfigurationRoot Get(string path, string environmentName = null, bool addUserSecrets = false)
+        {
+            var cacheKey = path + "#" + environmentName + "#" + addUserSecrets;
+            return _configurationCache.GetOrAdd(
+                cacheKey,
+                _ => BuildConfiguration(path, environmentName, addUserSecrets)
+            );
+        }
+
+        private static IConfigurationRoot BuildConfiguration(string path, string environmentName = null, bool addUserSecrets = false)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            if (string.IsNullOrWhiteSpace(environmentName))
+            {
+                builder = builder.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
+            }
+
+            builder = builder.AddEnvironmentVariables();
+
+            if (addUserSecrets)
+            {
+                builder.AddUserSecrets(Assembly.GetExecutingAssembly());
+            }
+
+            return builder.Build();
+        }
+    }
+
+
+}
