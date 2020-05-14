5c28a2407180b7469c394c23fd3eceab9ee9ce0c
Roc <roc@essensoft.cn>
2020-05-14 14:26:46 +0800

    优化配置选项提示注释

--- src/Essensoft.AspNetCore.Payment.WeChatPay/WeChatPayOptions.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/WeChatPayOptions.cs
@@ -2,6 +2,9 @@
 
 namespace Essensoft.AspNetCore.Payment.WeChatPay
 {
+    /// <summary>
+    /// WeChatPay 配置选项
+    /// </summary>
     public class WeChatPayOptions
     {
         internal string CertificateHash;
@@ -10,37 +13,41 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay
         private string certificatePassword;
 
         /// <summary>
-        /// 微信应用密钥(企业微信Secret，目前仅"企业红包API"使用)
+        /// 应用密钥
+        /// 如：目前仅调用"企业红包API"时，使用企业微信的Secret。
         /// </summary>
         public string Secret { get; set; }
 
         /// <summary>
-        /// 微信应用号(公众平台AppId/开放平台AppId/小程序AppId/企业微信CorpId)
+        /// 应用号
+        /// 如：公众平台AppId/开放平台AppId/小程序AppId/企业微信CorpId等
         /// </summary>
         public string AppId { get; set; }
 
         /// <summary>
-        /// 微信支付 子商户应用号(仅服务商时使用)
-        /// </summary>
-        public string SubAppId { get; set; }
-
-        /// <summary>
-        /// 微信支付 商户号
+        /// 商户号
         /// </summary>
         public string MchId { get; set; }
 
         /// <summary>
-        /// 微信支付 子商户号(仅服务商时使用)
+        /// 子商户应用号
+        /// 仅服务商时使用
+        /// </summary>
+        public string SubAppId { get; set; }
+
+        /// <summary>
+        /// 子商户号
+        /// 仅服务商时使用
         /// </summary>
         public string SubMchId { get; set; }
 
         /// <summary>
-        /// 微信支付 API密钥(不是APIv3密钥)
+        /// API密钥
         /// </summary>
         public string Key { get; set; }
 
         /// <summary>
-        /// 微信支付 API证书(文件名/文件的Base64编码)
+        /// API证书(文件名/文件的Base64编码)
         /// </summary>
         public string Certificate
         {
@@ -56,7 +63,8 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay
         }
 
         /// <summary>
-        /// 微信支付 API证书密码(默认为商户号)
+        /// API证书密码
+        /// 默认为商户号
         /// </summary>
         public string CertificatePassword
         {
@@ -65,7 +73,8 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay
         }
 
         /// <summary>
-        /// 微信支付 RSA公钥(目前仅"企业付款到银行卡API"使用，调用"获取RSA加密公钥API"即可获取)
+        /// RSA公钥
+        /// 目前仅调用"企业付款到银行卡API"时使用，执行"获取RSA加密公钥API"即可获取。
         /// </summary>
         public string RsaPublicKey { get; set; }
     }
