caa66ef4c6beae627efa39114b85a93148472daa
Roc <roc@essenol.com>
2020-06-11 20:39:59 +0800

    新增 H5下单API

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayTransactionsH5Request.cs
@@ -0,0 +1,29 @@
+﻿using Essensoft.AspNetCore.Payment.WeChatPay.Response;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
+{
+    /// <summary>
+    /// H5下单API
+    /// 最新更新时间：2020.05.26
+    /// https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transactions/chapter3_4.shtml
+    /// </summary>
+    public class WeChatPayTransactionsH5Request : IWeChatPayV3PostRequest<WeChatPayTransactionsH5Response>
+    {
+        private WeChatPayObject bizModel;
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/v3/pay/transactions/h5";
+        }
+
+        public WeChatPayObject GetBizModel()
+        {
+            return bizModel;
+        }
+
+        public void SetBizModel(WeChatPayObject bizModel)
+        {
+            this.bizModel = bizModel;
+        }
+    }
+}
