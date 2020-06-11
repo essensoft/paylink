701d377242bbbcf4373db463f949ec930d4cba1c
Roc <roc@essensoft.cn>
2020-06-11 14:47:44 +0800

    补充部分注释

--- src/Essensoft.AspNetCore.Payment.WeChatPay/Domain/Certificate.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Domain/Certificate.cs
@@ -1,21 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace Essensoft.AspNetCore.Payment.WeChatPay.Domain
 {
+    /// <summary>
+    /// 平台证书信息
+    /// </summary>
     public class Certificate : WeChatPayObject
     {
+        /// <summary>
+        /// 序列号
+        /// </summary>
         [JsonPropertyName("serial_no")]
         public string SerialNo { get; set; }
 
+        /// <summary>
+        /// 生效时间
+        /// </summary>
         [JsonPropertyName("effective_time")]
         public string EffectiveTime { get; set; }
 
+        /// <summary>
+        /// 失效时间
+        /// </summary>
         [JsonPropertyName("expire_time")]
         public string ExpireTime { get; set; }
 
+        /// <summary>
+        /// 加密证书信息
+        /// </summary>
         [JsonPropertyName("encrypt_certificate")]
         public EncryptCertificate EncryptCertificate { get; set; }
     }
