8fcaa9e0f72620e3dde0f903f7d3bc7554518d5f
Roc <roc@essenol.com>
2020-06-11 14:47:44 +0800

    补充部分注释

--- src/Essensoft.AspNetCore.Payment.WeChatPay/Response/WeChatPayTransactionsNativeResponse.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Response/WeChatPayTransactionsNativeResponse.cs
@@ -2,8 +2,19 @@
 
 namespace Essensoft.AspNetCore.Payment.WeChatPay.Response
 {
+    /// <summary>
+    /// Native下单API-返回参数
+    /// 最新更新时间：2020.05.26
+    /// https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transactions/chapter3_3.shtml
+    /// </summary>
     public class WeChatPayTransactionsNativeResponse : WeChatPayV3Response
     {
+        /// <summary>
+        /// 二维码链接
+        /// 此URL用于生成支付二维码，然后提供给用户扫码支付。
+        /// 注意：code_url并非固定值，使用时按照URL格式转成二维码即可。
+        /// 示例值：weixin://wxpay/bizpayurl/up?pr=NwY5Mz9&groupid=00
+        /// </summary>
         [JsonPropertyName("code_url")]
         public string CodeUrl { get; set; }
     }
