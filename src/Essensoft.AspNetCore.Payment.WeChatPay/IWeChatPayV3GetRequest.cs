ee1b3f640c8da458f78458e4f042737a84230f76
Roc <roc@essensoft.cn>
2020-06-12 16:59:29 +0800

    .

--- src/Essensoft.AspNetCore.Payment.WeChatPay/IWeChatPayV3GetRequest.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/IWeChatPayV3GetRequest.cs
@@ -1,21 +1,10 @@
 ﻿namespace Essensoft.AspNetCore.Payment.WeChatPay
 {
-    public interface IWeChatPayV3PostRequest<T> where T : WeChatPayV3Response
+    public interface IWeChatPayV3GetRequest<T> where T : WeChatPayV3Response
     {
         /// <summary>
         /// 请求URL
         /// </summary>
         string GetRequestUrl();
-
-        /// <summary>
-        /// 获取BizModel
-        /// </summary>
-        WeChatPayObject GetBizModel();
-
-        /// <summary>
-        /// 设置BizModel
-        /// </summary>
-        /// <param name="bizModel"></param>
-        void SetBizModel(WeChatPayObject bizModel);
     }
 }
