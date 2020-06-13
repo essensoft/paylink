2c099a16d55372884b7e8bfc883b816909a1cfd1
Roc <roc@essenol.com>
2020-06-13 16:04:15 +0800

    WeChatPayV3Response 新增 code、detail、message

--- src/Essensoft.AspNetCore.Payment.WeChatPay/WeChatPayV3Response.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/WeChatPayV3Response.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using Essensoft.AspNetCore.Payment.WeChatPay.Domain;
 
 namespace Essensoft.AspNetCore.Payment.WeChatPay
 {
@@ -15,5 +16,23 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay
         /// </summary>
         [JsonIgnore]
         public int StatusCode { get; set; }
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 错误详情
+        /// </summary>
+        [JsonPropertyName("detail")]
+        public ErrorDetail Detail { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
     }
 }
