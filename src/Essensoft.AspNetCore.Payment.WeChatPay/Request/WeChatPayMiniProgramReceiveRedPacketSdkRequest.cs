18ae5518e2f4b935a3d430aa73fdfbaf3b326161
Roc <roc@essenol.com>
2020-06-14 01:43:07 +0800

    LiteApp 修改为 MiniProgram

--- src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayMiniProgramReceiveRedPacketSdkRequest.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayMiniProgramReceiveRedPacketSdkRequest.cs
@@ -4,12 +4,12 @@ using Essensoft.AspNetCore.Payment.WeChatPay.Utility;
 namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
 {
     /// <summary>
-    /// 小程序调起支付
+    /// 小程序红包 - 领取红包
     /// </summary>
-    public class WeChatPayLiteAppSdkRequest : IWeChatPaySdkRequest
+    public class WeChatPayMiniProgramReceiveRedPacketSdkRequest : IWeChatPaySdkRequest
     {
         /// <summary>
-        /// 扩展字段
+        /// 红包详情的扩展
         /// </summary>
         public string Package { get; set; }
 
@@ -28,15 +28,6 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
         {
             sortedTxtParams.Add(WeChatPayConsts.timeStamp, WeChatPayUtility.GetTimeStamp());
             sortedTxtParams.Add(WeChatPayConsts.nonceStr, WeChatPayUtility.GenerateNonceStr());
-
-            if (!string.IsNullOrEmpty(options.SubAppId))
-            {
-                sortedTxtParams.Add(WeChatPayConsts.appId, options.SubAppId);
-            }
-            else
-            {
-                sortedTxtParams.Add(WeChatPayConsts.appId, options.AppId);
-            }
 
             sortedTxtParams.Add(WeChatPayConsts.signType, WeChatPayConsts.MD5);
             sortedTxtParams.Add(WeChatPayConsts.paySign, WeChatPaySignature.SignWithKey(sortedTxtParams, options.Key, WeChatPaySignType.MD5));
--- src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayMiniProgramSdkRequest.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayMiniProgramSdkRequest.cs
@@ -4,12 +4,12 @@ using Essensoft.AspNetCore.Payment.WeChatPay.Utility;
 namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
 {
     /// <summary>
-    /// 小程序红包 - 领取红包
+    /// 小程序调起支付
     /// </summary>
-    public class WeChatPayLiteAppReceiveRedPacketSdkRequest : IWeChatPaySdkRequest
+    public class WeChatPayMiniProgramSdkRequest : IWeChatPaySdkRequest
     {
         /// <summary>
-        /// 红包详情的扩展
+        /// 扩展字段
         /// </summary>
         public string Package { get; set; }
 
@@ -28,6 +28,15 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
         {
             sortedTxtParams.Add(WeChatPayConsts.timeStamp, WeChatPayUtility.GetTimeStamp());
             sortedTxtParams.Add(WeChatPayConsts.nonceStr, WeChatPayUtility.GenerateNonceStr());
+
+            if (!string.IsNullOrEmpty(options.SubAppId))
+            {
+                sortedTxtParams.Add(WeChatPayConsts.appId, options.SubAppId);
+            }
+            else
+            {
+                sortedTxtParams.Add(WeChatPayConsts.appId, options.AppId);
+            }
 
             sortedTxtParams.Add(WeChatPayConsts.signType, WeChatPayConsts.MD5);
             sortedTxtParams.Add(WeChatPayConsts.paySign, WeChatPaySignature.SignWithKey(sortedTxtParams, options.Key, WeChatPaySignType.MD5));
