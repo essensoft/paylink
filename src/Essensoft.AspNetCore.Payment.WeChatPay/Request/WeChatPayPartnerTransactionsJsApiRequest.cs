9f5964dbee2b4d2ac8302fce1968788feecb22be
Roc <roc@essenol.com>
2020-06-14 01:11:33 +0800

    实现 微信支付V3-基础支付-普通支付（服务商模式）API

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayPartnerTransactionsJsApiRequest.cs
@@ -0,0 +1,29 @@
+﻿using Essensoft.AspNetCore.Payment.WeChatPay.Response;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
+{
+    /// <summary>
+    /// JSAPI下单API（服务商）
+    /// 最新更新时间：2020.05.26
+    /// https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transactions/chapter5_2.shtml
+    /// </summary>
+    public class WeChatPayPartnerTransactionsJsApiRequest : IWeChatPayV3PostRequest<WeChatPayPartnerTransactionsJsApiResponse>
+    {
+        private WeChatPayObject queryModel;
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/v3/pay/partner/transactions/jsapi";
+        }
+
+        public WeChatPayObject GetQueryModel()
+        {
+            return queryModel;
+        }
+
+        public void SetQueryModel(WeChatPayObject queryModel)
+        {
+            this.queryModel = queryModel;
+        }
+    }
+}
