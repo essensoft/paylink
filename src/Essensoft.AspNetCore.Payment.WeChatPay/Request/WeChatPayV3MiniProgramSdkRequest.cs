dff22b914d1f4c8d79c3837b2f23d77d686f69ba
Roc <roc@essenol.com>
2020-06-15 01:45:10 +0800

    新增 小程序调起支付API

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/Request/WeChatPayV3MiniProgramSdkRequest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Essensoft.AspNetCore.Payment.Security;
+using Essensoft.AspNetCore.Payment.WeChatPay.Utility;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.Request
+{
+    /// <summary>
+    /// 小程序调起支付API
+    /// 最新更新时间：2020.03.23
+    /// https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/combine/chapter3_8.shtml
+    /// </summary>
+    public class WeChatPayV3MiniProgramSdkRequest : IWeChatPayV3SdkRequest
+    {
+        /// <summary>
+        /// 订单详情扩展字符串
+        /// </summary>
+        public string Package { get; set; }
+
+        #region IWeChatPayV3SdkRequest Members
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WeChatPayDictionary
+            {
+                { "package", Package }
+            };
+            return parameters;
+        }
+
+        public void PrimaryHandler(WeChatPayOptions options, WeChatPayDictionary sortedTxtParams)
+        {
+            if (!string.IsNullOrEmpty(options.SubAppId))
+            {
+                sortedTxtParams.Add(WeChatPayConsts.appId, options.SubAppId);
+            }
+            else
+            {
+                sortedTxtParams.Add(WeChatPayConsts.appId, options.AppId);
+            }
+
+            sortedTxtParams.Add(WeChatPayConsts.timeStamp, WeChatPayUtility.GetTimeStamp());
+            sortedTxtParams.Add(WeChatPayConsts.nonceStr, WeChatPayUtility.GenerateNonceStr());
+            sortedTxtParams.Add(WeChatPayConsts.signType, WeChatPayConsts.RSA);
+
+            var signatureSourceDate = BuildSignatureSourceDate(sortedTxtParams);
+            sortedTxtParams.Add(WeChatPayConsts.paySign, options.CertificateRSAPrivateKey.Sign(signatureSourceDate));
+        }
+
+        private static string BuildSignatureSourceDate(WeChatPayDictionary sortedTxtParams)
+        {
+            return $"{sortedTxtParams.GetValue(WeChatPayConsts.appId)}\n{sortedTxtParams.GetValue(WeChatPayConsts.timeStamp)}\n{sortedTxtParams.GetValue(WeChatPayConsts.nonceStr)}\n{sortedTxtParams.GetValue(WeChatPayConsts.package)}\n";
+        }
+
+        #endregion
+    }
+}
