b1051209ae5de210794c0b06a13a16e20cf18449
Roc <roc@essensoft.cn>
2020-07-10 11:05:34 +0800

    重命名 IWeChatPayNotifyClient 方法名

--- src/Essensoft.AspNetCore.Payment.WeChatPay/V3/IWeChatPayNotifyClient.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/V3/IWeChatPayNotifyClient.cs
@@ -15,7 +15,7 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.V3
         /// <param name="request">控制器的请求</param>
         /// <param name="options">配置选项</param>
         /// <returns>领域对象</returns>
-        Task<T> ExecuteV3Async<T>(Microsoft.AspNetCore.Http.HttpRequest request, WeChatPayOptions options) where T : WeChatPayNotify;
+        Task<T> ExecuteAsync<T>(Microsoft.AspNetCore.Http.HttpRequest request, WeChatPayOptions options) where T : WeChatPayNotify;
 #endif
         /// <summary>
         /// 执行 WeChatPay V3 通知请求解析
@@ -28,6 +28,6 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.V3
         /// <param name="signature">Wechatpay_Signature</param>
         /// <param name="options">配置选项</param>
         /// <returns>领域对象</returns>
-        Task<T> ExecuteV3Async<T>(string body, string serial, string timestamp, string nonce, string signature, WeChatPayOptions options) where T : WeChatPayNotify;
+        Task<T> ExecuteAsync<T>(string body, string serial, string timestamp, string nonce, string signature, WeChatPayOptions options) where T : WeChatPayNotify;
     }
 }
