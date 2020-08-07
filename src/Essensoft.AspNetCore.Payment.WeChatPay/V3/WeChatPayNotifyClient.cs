3560cf24316f41283c53972241e70e9b0a284f47
Roc <roc@essensoft.cn>
2020-08-07 14:27:55 +0800

    允许 多次读取 request.Body

--- src/Essensoft.AspNetCore.Payment.WeChatPay/V3/WeChatPayNotifyClient.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/V3/WeChatPayNotifyClient.cs
@@ -35,10 +35,13 @@ namespace Essensoft.AspNetCore.Payment.WeChatPay.V3
                 throw new ArgumentNullException(nameof(options));
             }
 
+            request.Body.Seek(0, SeekOrigin.Begin);
             var headers = GetWeChatPayHeadersFromRequest(request);
-            var body = await new StreamReader(request.Body, Encoding.UTF8).ReadToEndAsync();
-
-            return await ExecuteAsync<T>(headers, body, options);
+            using (var reader = new StreamReader(request.Body, Encoding.UTF8))
+            {
+                var body = await reader.ReadToEndAsync();
+                return await ExecuteAsync<T>(headers, body, options);
+            }
         }
 
         private WeChatPayHeaders GetWeChatPayHeadersFromRequest(Microsoft.AspNetCore.Http.HttpRequest request)
