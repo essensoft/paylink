ba4ccb5bff5f0d14d81883ebbbe4a733b77326c1
Roc <roc@essensoft.cn>
2020-12-16 21:03:40 +0800

    新增 AlipayMobilePublicMultiMediaClient 支持

---
+++ src/Essensoft.AspNetCore.Payment.Alipay/AlipayOpenAppAlipaycertDownloadRequest.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+
+namespace Essensoft.AspNetCore.Payment.Alipay
+{
+    public class AlipayOpenAppAlipaycertDownloadRequest : IAlipayRequest<AlipayOpenAppAlipaycertDownloadResponse>
+    {
+        public string BizContent { get; set; }
+
+        private string apiVersion = "1.0";
+        private string terminalType;
+        private string terminalInfo;
+        private string prodCode;
+        private string notifyUrl;
+        private string returnUrl;
+        private bool needEncrypt = false;
+        private AlipayObject bizModel;
+
+        public void SetNeedEncrypt(bool needEncrypt)
+        {
+            this.needEncrypt = needEncrypt;
+        }
+
+
+        public bool GetNeedEncrypt()
+        {
+            return needEncrypt;
+        }
+
+        public void SetNotifyUrl(string notifyUrl)
+        {
+            this.notifyUrl = notifyUrl;
+        }
+
+        public string GetNotifyUrl()
+        {
+            return notifyUrl;
+        }
+
+        public void SetReturnUrl(string returnUrl)
+        {
+            this.returnUrl = returnUrl;
+        }
+
+        public string GetReturnUrl()
+        {
+            return returnUrl;
+        }
+
+        public void SetApiVersion(string apiVersion)
+        {
+            this.apiVersion = apiVersion;
+        }
+
+        public string GetApiVersion()
+        {
+            return apiVersion;
+        }
+
+        public void SetTerminalType(string terminalType)
+        {
+            this.terminalType = terminalType;
+        }
+
+        public string GetTerminalType()
+        {
+            return terminalType;
+        }
+
+        public void SetTerminalInfo(string terminalInfo)
+        {
+            this.terminalInfo = terminalInfo;
+        }
+
+        public string GetTerminalInfo()
+        {
+            return terminalInfo;
+        }
+
+        public void SetProdCode(string prodCode)
+        {
+            this.prodCode = prodCode;
+        }
+
+        public string GetProdCode()
+        {
+            return prodCode;
+        }
+
+        public string GetApiName()
+        {
+            return "alipay.open.app.alipaycert.download";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new AlipayDictionary
+            {
+                { "biz_content", BizContent }
+            };
+            return parameters;
+        }
+
+        public AlipayObject GetBizModel()
+        {
+            return bizModel;
+        }
+
+        public void SetBizModel(AlipayObject bizModel)
+        {
+            this.bizModel = bizModel;
+        }
+    }
+}
---
+++ src/Essensoft.AspNetCore.Payment.Alipay/IAlipayMobilePublicMultiMediaClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Essensoft.AspNetCore.Payment.Alipay
+{
+    public interface IAlipayMobilePublicMultiMediaClient
+    {
+        /// <summary>
+        /// 执行 Alipay API请求。
+        /// </summary>
+        /// <param name="request">请求对象</param>
+        /// <param name="options">配置选项</param>
+        /// <returns>响应对象</returns>
+        Task<T> ExecuteAsync<T>(IAlipayRequest<T> request, AlipayOptions options) where T : AlipayResponse;
+
+        /// <summary>
+        /// 执行 Alipay API请求。
+        /// </summary>
+        /// <param name="request">请求对象</param>
+        /// <param name="options">配置选项</param>
+        /// <param name="accessToken">用户授权token</param>
+        /// <returns>响应对象</returns>
+        Task<T> ExecuteAsync<T>(IAlipayRequest<T> request, AlipayOptions options, string accessToken) where T : AlipayResponse;
+
+        /// <summary>
+        /// 执行 Alipay API请求。
+        /// </summary>
+        /// <param name="request">请求对象</param>
+        /// <param name="options">配置选项</param>
+        /// <param name="accessToken">用户授权token</param>
+        /// <param name="appAuthToken">三方应用授权token</param>
+        /// <returns>响应对象</returns>
+        Task<T> ExecuteAsync<T>(IAlipayRequest<T> request, AlipayOptions options, string accessToken, string appAuthToken) where T : AlipayResponse;
+    }
+}
