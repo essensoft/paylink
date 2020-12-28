3f69f80a1ec01029bec36e36c53eec8eedf1db1d
Roc <roc@essenol.com>
2020-12-28 20:09:11 +0800

    新增 AlipayTradeRefundDepositbackCompletedNotify、AlipayUserCertifyOpenNotifyCompletedNotify 修正注释

--- src/Essensoft.AspNetCore.Payment.Alipay/Notify/AlipayOpenAuthAppauthCancelledNotify.cs
+++ src/Essensoft.AspNetCore.Payment.Alipay/Notify/AlipayOpenAuthAppauthCancelledNotify.cs
@@ -1,6 +1,7 @@
 ﻿namespace Essensoft.AspNetCore.Payment.Alipay.Notify
 {
     /// <summary>
+    /// alipay.open.auth.appauth.cancelled
     /// 第三方应用授权取消消息
     /// https://docs.open.alipay.com/msgapi_54/alipay.open.auth.appauth.cancelled/
     /// </summary>
--- src/Essensoft.AspNetCore.Payment.Alipay/Notify/AlipayOpenAuthUserauthCancelledNotify.cs
+++ src/Essensoft.AspNetCore.Payment.Alipay/Notify/AlipayOpenAuthUserauthCancelledNotify.cs
@@ -1,6 +1,7 @@
 ﻿namespace Essensoft.AspNetCore.Payment.Alipay.Notify
 {
     /// <summary>
+    /// alipay.open.auth.userauth.cancelled
     /// 用户授权取消消息
     /// https://docs.open.alipay.com/msgapi_54/alipay.open.auth.userauth.cancelled/
     /// </summary>
