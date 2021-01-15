4b9d43abf3cd1a8995bb932d41e0955d012bd5f4
Roc <roc@essensoft.cn>
2021-01-15 12:37:23 +0800

    .

--- src/Essensoft.AspNetCore.Payment.WeChatPay/V3/Response/WeChatPayCertificatesResponse.cs
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/V3/Response/WeChatPayCertificatesResponse.cs
@@ -4,6 +4,10 @@ using Essensoft.AspNetCore.Payment.WeChatPay.V3.Domain;
 
 namespace Essensoft.AspNetCore.Payment.WeChatPay.V3.Response
 {
+    /// <summary>
+    /// 获取平台证书列表
+    /// https://wechatpay-api.gitbook.io/wechatpay-api-v3/jie-kou-wen-dang/ping-tai-zheng-shu
+    /// </summary>
     public class WeChatPayCertificatesResponse : WeChatPayResponse
     {
         [JsonPropertyName("data")]
