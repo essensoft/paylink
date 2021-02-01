f23efd802a2103bcb729c4dd09d8543870cee146
Roc <roc@essensoft.cn>
2021-02-01 16:48:14 +0800

    [WebApplicationSample] 新增查询单笔退款

--- samples/WebApplicationSample/Models/WeChatPayViewModel.cs
+++ samples/WebApplicationSample/Models/WeChatPayViewModel.cs
@@ -385,4 +385,20 @@ namespace WebApplicationSample.Models
         [Display(Name = "currency")]
         public string Currency { get; set; }
     }
+
+    public class WeChatPayV3RefundQueryViewModel
+    {
+        [Display(Name = "refund_id")]
+        public string RefundId { get; set; }
+
+        [Display(Name = "out_refund_no")]
+        public string OutRefundNo { get; set; }
+
+        [Display(Name = "transaction_id")]
+        public string TransactionId { get; set; }
+
+        [Display(Name = "out_trade_no")]
+        public string OutTradeNo { get; set; }
+    }
+
 }
