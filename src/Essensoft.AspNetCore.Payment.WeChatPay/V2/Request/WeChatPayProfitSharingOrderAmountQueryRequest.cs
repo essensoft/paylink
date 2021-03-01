d42991e0e5e1f7eed7976d417fa9098ae83e6758
Roc <roc@essenol.com>
2021-03-02 01:37:47 +0800

    [WeChatPay.V2] 新增 分账 - 查询订单待分账金额

---
+++ src/Essensoft.AspNetCore.Payment.WeChatPay/V2/Request/WeChatPayProfitSharingOrderAmountQueryRequest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Essensoft.AspNetCore.Payment.WeChatPay.V2.Response;
+
+namespace Essensoft.AspNetCore.Payment.WeChatPay.V2.Request
+{
+    /// <summary>
+    /// 分账 - 查询订单待分账金额 (普通商户 / 服务商)
+    /// </summary>
+    public class WeChatPayProfitSharingOrderAmountQueryRequest : IWeChatPayRequest<WeChatPayProfitSharingOrderAmountQueryResponse>
+    {
+        /// <summary>
+        /// 微信订单号
+        /// </summary>
+        public string TransactionId { get; set; }
+
+        #region IWeChatPayRequest Members
+
+        private string requestUrl = "https://api.mch.weixin.qq.com/pay/profitsharingorderamountquery";
+
+        public string GetRequestUrl()
+        {
+            return requestUrl;
+        }
+
+        public void SetRequestUrl(string url)
+        {
+            requestUrl = url;
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WeChatPayDictionary
+            {
+                { "transaction_id", TransactionId },
+            };
+            return parameters;
+        }
+
+        public WeChatPaySignType GetSignType()
+        {
+            return WeChatPaySignType.HMAC_SHA256;
+        }
+
+        public void PrimaryHandler(WeChatPayOptions options, WeChatPaySignType signType, WeChatPayDictionary sortedTxtParams)
+        {
+            sortedTxtParams.Add(WeChatPayConsts.nonce_str, WeChatPayUtility.GenerateNonceStr());
+            sortedTxtParams.Add(WeChatPayConsts.mch_id, options.MchId);
+
+            if (signType == WeChatPaySignType.HMAC_SHA256)
+            {
+                sortedTxtParams.Add(WeChatPayConsts.sign_type, WeChatPayConsts.HMAC_SHA256);
+            }
+
+            sortedTxtParams.Add(WeChatPayConsts.sign, WeChatPaySignature.SignWithKey(sortedTxtParams, options.Key, signType));
+        }
+
+        #endregion
+    }
+}
