ca4a758a1354449482be5529d9907e09cc3bad00
Roc <roc@essensoft.cn>
2021-03-18 14:42:09 +0800

    [WeChatPay.V3] 调整部分summary

--- src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayRefundDomesticRefundsRequest.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayRefundDomesticRefundsRequest.cs
@@ -3,8 +3,7 @@
 namespace Essensoft.Paylink.WeChatPay.V3.Request
 {
     /// <summary>
-    /// <para>基础支付 - JSAPI支付、APP支付、H5支付、Native支付、小程序支付 - 申请退款</para>
-    /// <para>基础支付(服务商) - JSAPI支付、APP支付、H5支付、Native支付、小程序支付 - 申请退款</para>
+    /// 基础支付、基础支付(服务商) - JSAPI支付、APP支付、H5支付、Native支付、小程序支付 - 申请退款
     /// </summary>
     /// <remarks>
     /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter3_1_9.shtml">基础支付 - JSAPI支付 - 申请退款API</a> - 最新更新时间：2021.01.15</para>
