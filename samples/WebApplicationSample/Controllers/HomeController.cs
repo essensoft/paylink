a73d8dc77b594cfe7bc95412ef97ad6f5a32d563
Roc <roc@essenol.com>
2021-03-23 03:28:01 +0800

    [WebApplicationSample] 调整 QRCode

--- samples/WebApplicationSample/Controllers/HomeController.cs
+++ samples/WebApplicationSample/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using System.Diagnostics;
-using System.Drawing;
-using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using QRCoder;
 using WebApplicationSample.Models;
 
 namespace WebApplicationSample.Controllers
@@ -15,23 +12,6 @@ namespace WebApplicationSample.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        /// <summary>
-        /// 生成二维码SVG
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="size"></param>
-        [HttpGet]
-        public IActionResult QrCode(string data, int size = 168)
-        {
-            using (var qrGenerator = new QRCodeGenerator())
-            using (var qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.L))
-            using (var svgQrCode = new SvgQRCode(qrCodeData))
-            {
-                var svgText = svgQrCode.GetGraphic(new Size(size, size));
-                return File(Encoding.UTF8.GetBytes(svgText), "text/xml");
-            }
         }
 
         public IActionResult Error()
