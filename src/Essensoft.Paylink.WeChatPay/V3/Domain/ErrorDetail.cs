ca18071172074f56da58136da0a0787e6ebb5296
Roc <roc@essenol.com>
2021-04-01 20:33:52 +0800

    [WeChatPay.V3] 根据微信文档 调整错误详情字段

--- src/Essensoft.Paylink.WeChatPay/V3/Domain/ErrorDetail.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Domain/ErrorDetail.cs
@@ -7,10 +7,25 @@ namespace Essensoft.Paylink.WeChatPay.V3.Domain
     /// </summary>
     public class ErrorDetail : WeChatPayObject
     {
+        /// <summary>
+        /// 指示错误参数的位置。当错误参数位于请求body的JSON时，填写指向参数的JSON Pointer 。当错误参数位于请求的url或者querystring时，填写参数的变量名。
+        /// </summary>
+        [JsonPropertyName("field")]
+        public string Field { get; set; }
+
+        /// <summary>
+        /// 错误的值
+        /// </summary>
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 具体错误原因
+        /// </summary>
+        [JsonPropertyName("issue")]
+        public string Issue { get; set; }
+
         [JsonPropertyName("location")]
         public string Location { get; set; }
-
-        [JsonPropertyName("value")]
-        public int Value { get; set; }
     }
 }
