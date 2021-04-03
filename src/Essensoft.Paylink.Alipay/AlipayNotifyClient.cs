6e9e45086564ee5a03cbd9f511727c2d397e68ab
Roc <roc@essenol.com>
2021-04-03 14:52:59 +0800

    Revert "移除 *NotifyClient、I*NotifyClient类，相关方法分别合并至*Client、I*Client类。"
    This reverts commit 218f18600f21d0f65a8c85365e4c60b105c6c5ca.

---
+++ src/Essensoft.Paylink.Alipay/AlipayNotifyClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Essensoft.Paylink.Alipay.Parser;
+using Essensoft.Paylink.Alipay.Utility;
+
+namespace Essensoft.Paylink.Alipay
+{
+    public class AlipayNotifyClient : IAlipayNotifyClient
+    {
+        #region AlipayNotifyClient Constructors
+
+        public AlipayNotifyClient()
+        {
+
+        }
+
+        #endregion
+
+        #region IAlipayNotifyClient Members
+
+#if NETCOREAPP3_1 || NET5_0
+        public async Task<T> ExecuteAsync<T>(Microsoft.AspNetCore.Http.HttpRequest request, AlipayOptions options) where T : AlipayNotify
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (string.IsNullOrEmpty(options.SignType))
+            {
+                throw new AlipayException($"options.{nameof(AlipayOptions.SignType)} is Empty!");
+            }
+
+            if (string.IsNullOrEmpty(options.AlipayPublicKey))
+            {
+                throw new AlipayException($"options.{nameof(AlipayOptions.AlipayPublicKey)} is Empty!");
+            }
+
+            var parameters = await GetParametersAsync(request);
+            return await ExecuteAsync<T>(parameters, options);
+        }
+#endif
+
+        #endregion
+
+        #region IAlipayNotifyClient Members
+
+#if NETCOREAPP3_1 || NET5_0
+        public Task<T> CertificateExecuteAsync<T>(Microsoft.AspNetCore.Http.HttpRequest request, AlipayOptions options) where T : AlipayNotify
+        {
+            return ExecuteAsync<T>(request, options);
+        }
+#endif
+
+        #endregion
+
+        #region IAlipayNotifyClient Members
+
+#if NETCOREAPP3_1 || NET5_0
+        public async Task<IDictionary<string, string>> GetParametersAsync(Microsoft.AspNetCore.Http.HttpRequest request)
+        {
+            var parameters = new Dictionary<string, string>();
+            if (request.Method == "POST")
+            {
+                var form = await request.ReadFormAsync();
+                foreach (var iter in form)
+                {
+                    parameters.Add(iter.Key, iter.Value);
+                }
+            }
+            else
+            {
+                foreach (var iter in request.Query)
+                {
+                    parameters.Add(iter.Key, iter.Value);
+                }
+            }
+            return parameters;
+        }
+#endif
+
+        #endregion
+
+        #region IAlipayNotifyClient Members
+
+        public Task<T> ExecuteAsync<T>(IDictionary<string, string> parameters, AlipayOptions options) where T : AlipayNotify
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (string.IsNullOrEmpty(options.SignType))
+            {
+                throw new AlipayException($"options.{nameof(AlipayOptions.SignType)} is Empty!");
+            }
+
+            if (string.IsNullOrEmpty(options.AlipayPublicKey))
+            {
+                throw new AlipayException($"options.{nameof(AlipayOptions.AlipayPublicKey)} is Empty!");
+            }
+
+            var notify = AlipayDictionaryParser.Parse<T>(parameters);
+            CheckNotifySign(parameters, options);
+            return Task.FromResult(notify);
+        }
+
+        #endregion
+
+        #region IAlipayNotifyClient Members
+
+        public Task<T> CertificateExecuteAsync<T>(IDictionary<string, string> parameters, AlipayOptions options) where T : AlipayNotify
+        {
+            return ExecuteAsync<T>(parameters, options);
+        }
+
+        #endregion
+
+        #region Common Method
+
+        private static void CheckNotifySign(IDictionary<string, string> dictionary, AlipayOptions options)
+        {
+            if (dictionary == null || dictionary.Count == 0)
+            {
+                throw new AlipayException("sign check fail: dictionary)} is Empty!");
+            }
+
+            if (!dictionary.TryGetValue(AlipayConstants.SIGN, out var sign))
+            {
+                throw new AlipayException("sign check fail: sign)} is Empty!");
+            }
+
+            dictionary.Remove(AlipayConstants.SIGN);
+            dictionary.Remove(AlipayConstants.SIGN_TYPE);
+            var content = AlipaySignature.GetSignContent(dictionary);
+            if (!AlipaySignature.RSACheckContent(content, sign, options.AlipayPublicKey, options.SignType))
+            {
+                throw new AlipayException("sign check fail: check Sign and Data Fail!");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Essensoft.Paylink.WeChatPay/V3/WeChatPayNotifyClient.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Essensoft.Paylink.Security;
+using Essensoft.Paylink.WeChatPay.V3.Parser;
+
+namespace Essensoft.Paylink.WeChatPay.V3
+{
+    public class WeChatPayNotifyClient : IWeChatPayNotifyClient
+    {
+        #region WeChatPayNotifyClient Constructors
+
+        private readonly IWeChatPayClient _client;
+        private readonly WeChatPayPlatformCertificateManager _platformCertificateManager;
+
+        public WeChatPayNotifyClient(IWeChatPayClient client, WeChatPayPlatformCertificateManager platformCertificateManager)
+        {
+            _client = client;
+            _platformCertificateManager = platformCertificateManager;
+        }
+
+        #endregion
+
+        #region IWeChatPayNotifyClient Members
+
+#if NETCOREAPP3_1 || NET5_0
+        public async Task<T> ExecuteAsync<T>(Microsoft.AspNetCore.Http.HttpRequest request, WeChatPayOptions options) where T : WeChatPayNotify
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            var headers = GetWeChatPayHeadersFromRequest(request);
+            using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                var body = await reader.ReadToEndAsync();
+                return await ExecuteAsync<T>(headers, body, options);
+            }
+        }
+
+        private static WeChatPayHeaders GetWeChatPayHeadersFromRequest(Microsoft.AspNetCore.Http.HttpRequest request)
+        {
+            var headers = new WeChatPayHeaders();
+
+            if (request.Headers.TryGetValue(WeChatPayConsts.Wechatpay_Serial, out var serialValues))
+            {
+                headers.Serial = serialValues.First();
+            }
+
+            if (request.Headers.TryGetValue(WeChatPayConsts.Wechatpay_Timestamp, out var timestampValues))
+            {
+                headers.Timestamp = timestampValues.First();
+            }
+
+            if (request.Headers.TryGetValue(WeChatPayConsts.Wechatpay_Nonce, out var nonceValues))
+            {
+                headers.Nonce = nonceValues.First();
+            }
+
+            if (request.Headers.TryGetValue(WeChatPayConsts.Wechatpay_Signature, out var signatureValues))
+            {
+                headers.Signature = signatureValues.First();
+            }
+
+            return headers;
+        }
+#endif
+
+        #endregion
+
+        #region IWeChatPayNotifyClient Members
+
+        public async Task<T> ExecuteAsync<T>(WeChatPayHeaders headers, string body, WeChatPayOptions options) where T : WeChatPayNotify
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (string.IsNullOrEmpty(options.APIv3Key))
+            {
+                throw new WeChatPayException($"options.{nameof(options.APIv3Key)} is Empty!");
+            }
+
+            await CheckNotifySignAsync(headers, body, options);
+
+            var parser = new WeChatPayNotifyJsonParser<T>();
+            var notify = parser.Parse(body, options.APIv3Key);
+
+            return notify;
+        }
+
+        #endregion
+
+        #region Check Notify Method
+
+        private async Task CheckNotifySignAsync(WeChatPayHeaders headers, string body, WeChatPayOptions options)
+        {
+            if (string.IsNullOrEmpty(headers.Serial))
+            {
+                throw new WeChatPayException($"sign check fail: {nameof(headers.Serial)} is empty!");
+            }
+
+            if (string.IsNullOrEmpty(headers.Signature))
+            {
+                throw new WeChatPayException($"sign check fail: {nameof(headers.Signature)} is empty!");
+            }
+
+            if (string.IsNullOrEmpty(body))
+            {
+                throw new WeChatPayException("sign check fail: body is empty!");
+            }
+
+            var cert = await _platformCertificateManager.GetCertificateAsync(_client, options, headers.Serial);
+            var signSourceData = WeChatPayUtility.BuildSignatureSourceData(headers.Timestamp, headers.Nonce, body);
+            var signCheck = SHA256WithRSA.Verify(cert.Certificate.GetRSAPublicKey(), signSourceData, headers.Signature);
+            if (!signCheck)
+            {
+                throw new WeChatPayException("sign check fail: check Sign and Data Fail!");
+            }
+        }
+
+        #endregion
+    }
+}
