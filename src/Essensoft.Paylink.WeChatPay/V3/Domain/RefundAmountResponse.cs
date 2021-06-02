231ca72f2d77daf455727b1a82ac5fb636361ba2
Roc <roc@essensoft.cn>
2021-06-03 01:15:44 +0800

    [WeChatPay.V3] 《申请退款API》、《查询退款API》新增字段 from（退款出资账户及金额）

--- src/Essensoft.Paylink.WeChatPay/V3/Domain/RefundAmountResponse.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Domain/RefundAmountResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Essensoft.Paylink.WeChatPay.V3.Domain
 {
@@ -26,6 +27,22 @@ namespace Essensoft.Paylink.WeChatPay.V3.Domain
         /// </remarks>
         [JsonPropertyName("refund")]
         public int Refund { get; set; }
+
+        /// <summary>
+        /// 退款出资账户及金额
+        /// </summary>
+        /// <remarks>
+        /// 退款需要从指定账户出资时，传递此参数指定出资金额（币种的最小单位，只能为整数）。
+        /// 同时指定多个账户出资退款的使用场景需要满足以下条件：
+        ///   1、未开通退款支出分离产品功能；
+        ///   2、订单属于分账订单，且分账处于待分账或分账中状态。
+        /// 参数传递需要满足条件：
+        /// 1、基本账户可用余额出资金额与基本账户不可用余额出资金额之和等于退款金额；
+        /// 2、账户类型不能重复。
+        /// 上述任一条件不满足将返回错误
+        /// </remarks>
+        [JsonPropertyName("from")]
+        public List<RefundAmountFrom> Form { get; set; }
 
         /// <summary>
         /// 用户支付金额
