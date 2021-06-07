84a15741711c3fcddf2ec00bb00008a0f78cb028
Roc <roc@essenol.com>
2021-06-07 17:43:29 +0800

    [WeChatPay.V3] 实现 分账 - 请求分账回退

---
+++ src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayProfitSharingReturnOrdersRequest.cs
@@ -0,0 +1,31 @@
+﻿using Essensoft.Paylink.WeChatPay.V3.Response;
+
+namespace Essensoft.Paylink.WeChatPay.V3.Request
+{
+    /// <summary>
+    /// 资金应用 - 分账 - 请求分账回退
+    /// </summary>
+    /// <remarks>
+    /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter8_1_3.shtml">分账 - 请求分账回退API</a> - 最新更新时间：2021.05.11</para>
+    /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter8_1_3.shtml">分账(服务商) - 请求分账回退API</a> - 最新更新时间：2021.05.11</para>
+    /// </remarks>
+    public class WeChatPayProfitSharingReturnOrdersRequest : IWeChatPayPostRequest<WeChatPayProfitSharingReturnOrdersResponse>
+    {
+        private WeChatPayObject bodyModel;
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/v3/profitsharing/return-orders";
+        }
+
+        public WeChatPayObject GetBodyModel()
+        {
+            return bodyModel;
+        }
+
+        public void SetBodyModel(WeChatPayObject bodyModel)
+        {
+            this.bodyModel = bodyModel;
+        }
+    }
+}
