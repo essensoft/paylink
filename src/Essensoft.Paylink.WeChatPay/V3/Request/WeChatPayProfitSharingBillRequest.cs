60e7e79ef19c9e92ba930d37e4d5744cd36fbffe
Roc <roc@essensoft.cn>
2021-07-27 01:44:40 +0800

    [WeChatPay.V3] 实现  分账《申请分账账单API》

---
+++ src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayProfitSharingBillRequest.cs
@@ -0,0 +1,30 @@
+﻿using Essensoft.Paylink.WeChatPay.V3.Response;
+
+namespace Essensoft.Paylink.WeChatPay.V3.Request
+{
+    /// <summary>
+    /// 资金应用 - 分账 - 申请分账账单 - 请求URL参数
+    /// </summary>
+    /// <remarks>
+    /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter8_1_11.shtml">资金应用 - 分账 - 申请分账账单API</a> - 最新更新时间：2021.07.22</para>
+    /// </remarks>
+    public class WeChatPayProfitSharingBillRequest : IWeChatPayGetRequest<WeChatPayProfitSharingBillResponse>
+    {
+        private WeChatPayObject queryModel;
+
+        public string GetRequestUrl()
+        {
+            return "https://api.mch.weixin.qq.com/v3/profitsharing/bills";
+        }
+
+        public WeChatPayObject GetQueryModel()
+        {
+            return queryModel;
+        }
+
+        public void SetQueryModel(WeChatPayObject queryModel)
+        {
+            this.queryModel = queryModel;
+        }
+    }
+}
---
+++ src/Essensoft.Paylink.WeChatPay/V3/Response/WeChatPayProfitSharingBillResponse.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Essensoft.Paylink.WeChatPay.V3.Response
+{
+    /// <summary>
+    /// 资金应用 - 分账 - 申请分账账单 - 返回参数
+    /// </summary>
+    /// <remarks>
+    /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter8_1_11.shtml">资金应用 - 分账 - 申请分账账单API</a> - 最新更新时间：2021.07.22</para>
+    /// </remarks>
+    public class WeChatPayProfitSharingBillResponse : WeChatPayResponse
+    {
+        /// <summary>
+        /// 哈希类型
+        /// </summary>
+        /// <remarks>
+        /// 原始账单（gzip需要解压缩）的摘要值，用于校验文件的完整性。
+        /// <para>示例值：SHA1</para>
+        /// </remarks>
+        [JsonPropertyName("hash_type")]
+        public string HashType { get; set; }
+
+        /// <summary>
+        /// 哈希值
+        /// </summary>
+        /// <remarks>
+        /// 原始账单（gzip需要解压缩）的摘要值，用于校验文件的完整性。
+        /// <para>示例值：79bb0f45fc4c42234a918000b2668d689e2bde04</para>
+        /// </remarks>
+        [JsonPropertyName("hash_value")]
+        public string HashValue { get; set; }
+
+        /// <summary>
+        /// 账单下载地址
+        /// </summary>
+        /// <remarks>
+        /// 供下一步请求账单文件的下载地址，该地址30s内有效。
+        /// <para>示例值：https://api.mch.weixin.qq.com/v3/billdownload/file?token=xxx</para>
+        /// </remarks>
+        [JsonPropertyName("download_url")]
+        public string DownloadUrl { get; set; }
+    }
+}
