25a2fbd26a8433ce66a38c1a28f1164264314942
Roc <roc@essensoft.cn>
2021-11-05 16:42:45 +0800

    [WeChatPay.V3] 修正 WeChatPayProfitSharingReceiversAddRequest 不加密的问题

--- src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayProfitSharingReceiversAddRequest.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayProfitSharingReceiversAddRequest.cs
@@ -9,7 +9,7 @@ namespace Essensoft.Paylink.WeChatPay.V3.Request
     /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter8_1_8.shtml">分账 - 添加分账接收方API</a> - 最新更新时间：2021.05.11</para>
     /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter8_1_8.shtml">分账(服务商) - 添加分账接收方API</a> - 最新更新时间：2021.05.11</para>
     /// </remarks>
-    public class WeChatPayProfitSharingReceiversAddRequest : IWeChatPayPostRequest<WeChatPayProfitSharingReceiversAddResponse>
+    public class WeChatPayProfitSharingReceiversAddRequest : IWeChatPayPrivacyPostRequest<WeChatPayProfitSharingReceiversAddResponse>
     {
         private WeChatPayObject bodyModel;
 
