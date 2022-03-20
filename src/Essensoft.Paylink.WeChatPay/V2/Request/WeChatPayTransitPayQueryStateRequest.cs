d7406debffa1d1da6290fcebf404fbbba7c4b7c3
算神 <lishewen@msn.com>
2022-03-21 07:58:54 +0800

    增加 微信代扣 - 乘车码代扣 - 用户状态查询 (直连模式)

---
+++ src/Essensoft.Paylink.WeChatPay/V2/Request/WeChatPayTransitPayQueryStateRequest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Essensoft.Paylink.WeChatPay.V2.Response;
+
+namespace Essensoft.Paylink.WeChatPay.V2.Request
+{
+    /// <summary>
+    /// 微信代扣 - 乘车码代扣 - 用户状态查询 (直连模式)
+    /// </summary>
+    public class WeChatPayTransitPayQueryStateRequest : IWeChatPayRequest<WeChatPayTransitPayQueryStateResponse>
+    {
+        /// <summary>
+        /// 委托代扣协议id
+        /// </summary>
+        public string ContractId { get; set; }
+
+        /// <summary>
+        /// 微信OpenId
+        /// </summary>
+        public string OpenId { get; set; }
+
+        #region IWeChatPayRequest Members
+
+        private string requestUrl = "https://api.mch.weixin.qq.com/transit/pay/querystate";
+        private WeChatPaySignType signType = WeChatPaySignType.HMAC_SHA256;
+
+        public string GetRequestUrl()
+        {
+            return requestUrl;
+        }
+
+        public void SetRequestUrl(string url)
+        {
+            requestUrl = url;
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            var parameters = new WeChatPayDictionary
+            {
+                { "contract_id", ContractId },
+                { "openid", OpenId },
+            };
+            return parameters;
+        }
+
+        public WeChatPaySignType GetSignType()
+        {
+            return signType;
+        }
+
+        public void SetSignType(WeChatPaySignType signType)
+        {
+            this.signType = signType switch
+            {
+                WeChatPaySignType.HMAC_SHA256 => signType,
+                _ => throw new WeChatPayException("api only support HMAC_SHA256!"),
+            };
+        }
+
+        public void PrimaryHandler(WeChatPayDictionary sortedTxtParams, WeChatPayOptions options)
+        {
+            sortedTxtParams.Add(WeChatPayConsts.nonce_str, WeChatPayUtility.GenerateNonceStr());
+            sortedTxtParams.Add(WeChatPayConsts.appid, options.AppId);
+            sortedTxtParams.Add(WeChatPayConsts.mch_id, options.MchId);
+
+            sortedTxtParams.Add(WeChatPayConsts.sign_type, signType);
+            sortedTxtParams.Add(WeChatPayConsts.sign, WeChatPaySignature.SignWithKey(sortedTxtParams, options.APIKey, signType));
+        }
+
+        #endregion
+    }
+}
