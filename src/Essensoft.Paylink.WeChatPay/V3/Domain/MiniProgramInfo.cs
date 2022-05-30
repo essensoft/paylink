1f5bdf8e9f07ab4f94709ea215044c9ea763bdcf
yuning <84052096@qq.com>
2022-05-30 15:37:18 +0800

    修复商户进件缺少内容

---
+++ src/Essensoft.Paylink.WeChatPay/V3/Domain/MiniProgramInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Essensoft.Paylink.WeChatPay.V3.Domain
+{
+    /// <summary>
+    /// 小程序场景
+    /// </summary>
+    public class MiniProgramInfo
+    {
+        /// <summary>
+        /// 服务商小程序APPID
+        /// 1、服务商小程序APPID与商家小程序APPID，二选一必填。
+        /// 2、可填写当前服务商商户号已绑定的小程序APPID。
+        /// </summary>
+        [JsonPropertyName("mini_program_appid")]
+        public string MiniProgramAppid { get; set; }
+        /// <summary>
+        /// 商家小程序APPID
+        /// 1、服务商小程序APPID与商家小程序APPID，二选一必填；
+        /// 2、请填写已认证的小程序APPID；
+        /// 3、完成进件后，系统发起特约商户号与该AppID的绑定（即配置为sub_appid可在发起支付时传入）
+        ///  （1）若APPID主体与商家主体/服务商主体一致，则直接完成绑定；
+        ///  （2）若APPID主体与商家主体/服务商主体不一致，则商户签约时显示《联合营运承诺函》，并且AppID的管理员需登录公众平台确认绑定意愿；
+        /// </summary>
+        [JsonPropertyName("mini_program_sub_appid")]
+        public string MiniProgramSubAppid { get; set; }
+
+        /// <summary>
+        /// 小程序截图
+        /// 1、请提供展示商品/服务的页面截图/设计稿（最多5张），若公众号未建设完善或未上线请务必提供。
+        /// 2、请填写通过图片上传API预先上传图片生成好的MediaID。
+        /// </summary>
+        [JsonPropertyName("mini_program_pics")]
+        public List<string> MiniProgramPics { get; set; }
+    }
+}
---
+++ src/Essensoft.Paylink.WeChatPay/V3/Domain/WebInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Essensoft.Paylink.WeChatPay.V3.Domain
+{
+    /// <summary>
+    /// 互联网网站场景
+    /// </summary>
+    public class WebInfo
+    {
+        /// <summary>
+        /// 互联网网站域名
+        /// 1、如为PC端商城、智能终端等场景，可上传官网链接。
+        /// 2、网站域名需ICP备案，若备案主体与申请主体不同，请上传加盖公章的网站授权函。
+        /// </summary>
+        [JsonPropertyName("domain")]
+        public string Domain { get; set; }
+        /// <summary>
+        /// 网站授权函
+        ///1、若备案主体与申请主体不同，请务必上传加盖公章的网站授权函。
+        /// 2、请填写通过图片上传API预先上传图片生成好的MediaID。
+        /// </summary>
+        [JsonPropertyName("web_authorisation")]
+        public string WebAuthorisation { get; set; }
+
+        /// <summary>
+        /// 互联网网站对应的商家APPID
+        /// 1、可填写已认证的公众号、小程序、应用的APPID，其中公众号APPID需是已认证的服务 号、政府或媒体类型的订阅号；
+        /// 2、完成进件后，系统发起特约商户号与该AppID的绑定（即配置为sub_appid，可在发起支付时传入）
+        ///（1）若APPID主体与商家主体一致，则直接完成绑定；
+        /// （2）若APPID主体与商家主体不一致，则商户签约时显示《联合营运承诺函》，并且 AppID的管理员需登录公众平台确认绑定意愿；（ 暂不支持绑定异主体的应用APPID）
+        /// </summary>
+        [JsonPropertyName("web_appid")]
+        public List<string> WebAppid { get; set; }
+    }
+}
