f121b92ed29c32f021995d4c8e7dd52baefff2c7
roc <roc@essensoft.cn>
2023-05-15 16:13:54 +0800

    [WeChatPay.V3] 修正 WeChatPayProfitSharingOrdersUnfreezeBodyModel

--- src/Essensoft.Paylink.WeChatPay/V3/Domain/WeChatPayProfitSharingOrdersUnfreezeBodyModel.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Domain/WeChatPayProfitSharingOrdersUnfreezeBodyModel.cs
@@ -9,7 +9,7 @@ namespace Essensoft.Paylink.WeChatPay.V3.Domain
     /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter8_1_5.shtml">分账 - 解冻剩余资金API</a> - 最新更新时间：2021.05.11</para>
     /// <para><a href="https://pay.weixin.qq.com/wiki/doc/apiv3_partner/apis/chapter8_1_5.shtml">分账(服务商) - 解冻剩余资金API</a> - 最新更新时间：2021.05.11</para>
     /// </remarks>
-    public class WeChatPayProfitSharingOrdersUnfreezeResponse : WeChatPayObject
+    public class WeChatPayProfitSharingOrdersUnfreezeBodyModel : WeChatPayObject
     {
         /// <summary>
         /// 子商户号 
