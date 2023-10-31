3e07a03d654f567df752a0bf892785cf5f539804
Roc <roc@essensoft.cn>
2023-10-31 14:12:30 +0800

    Update IWeChatPayClient.cs closed#159

--- src/Essensoft.Paylink.WeChatPay/V3/IWeChatPayClient.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/IWeChatPayClient.cs
@@ -32,6 +32,14 @@ namespace Essensoft.Paylink.WeChatPay.V3
         Task<T> ExecuteAsync<T>(IWeChatPayPostRequest<T> request, WeChatPayOptions options) where T : WeChatPayResponse;
 
         /// <summary>
+        /// 执行 WeChatPay V3 Get敏感信息请求。
+        /// </summary>
+        /// <param name="request">请求对象</param>
+        /// <param name="options">配置选项</param>
+        /// <returns>响应对象</returns>
+        Task<T> ExecuteAsync<T>(IWeChatPayPrivacyGetRequest<T> request, WeChatPayOptions options) where T : WeChatPayResponse
+
+        /// <summary>
         /// 执行 WeChatPay V3 Post敏感信息请求。
         /// </summary>
         /// <param name="request">请求对象</param>
